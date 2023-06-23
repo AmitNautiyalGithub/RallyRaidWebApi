2e25a4ee0f2d5377e23addbae4f1d5482d4738e8
AmitNautiyal <59432251+AmitNautiyalGithub@users.noreply.github.com>
2023-06-23 22:50:34 +0530

    RRW-Feature Add EF and Configuration

---
+++ Domain/Entities/MotorBike.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class MotorBike
+    {
+        public int Id { get; set; }
+        public string TeamName { get; set; }
+        public double MelfunctionChance { get; set; }
+        public int MelfunctionOccured { get; set; }
+        public int Speed { get; set; }
+        public int DistanceConveredInMiles { get; set; }
+        public int FinishedRace { get; set; }
+        public int RacedForHours { get; set; }
+    }
+}
---
+++ WebUI/Features/MotoBikeController.cs
@@ -0,0 +1,85 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Features
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MotoBikeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<MotorBike>> GetCars()
+        {
+            var MotorBikes = new List<MotorBike>();
+            MotorBike MotorBike1 = new MotorBike
+            {
+                TeamName = "MotorBike Team A",
+                Speed = 100,
+                MelfunctionChance = 0.2,
+            };
+
+            MotorBike MotorBike2 = new MotorBike
+            {
+                TeamName = "MotorBike Team B",
+                Speed = 90,
+                MelfunctionChance = 0.15,
+            };
+
+            MotorBikes.Add(MotorBike1);
+            MotorBikes.Add(MotorBike2);
+
+            return Ok(MotorBikes);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult<MotorBike> GetMotorBike(int id)
+        {
+            MotorBike MotorBike1 = new MotorBike
+            {
+                TeamName = "MotorBike Team A",
+                Speed = 100,
+                MelfunctionChance = 0.2,
+            };
+
+            return Ok(MotorBike1);
+        }
+
+        [HttpPost]
+        public ActionResult<MotorBike> CreateMotorBike(MotorBike motorBike)
+        {
+            var newMotorBike = new MotorBike
+            {
+                Id = motorBike.Id,
+                TeamName = motorBike.TeamName,
+                Speed = motorBike.Speed,
+                MelfunctionChance = motorBike.MelfunctionChance
+            };
+
+            return Ok(newMotorBike);
+        }
+
+        [HttpPut]
+        public ActionResult<MotorBike> UpdateMotorBike(MotorBike motorBike)
+        {
+            var updateMotorBike = new MotorBike
+            {
+                Id = motorBike.Id,
+                TeamName = motorBike.TeamName,
+                Speed = motorBike.Speed,
+                MelfunctionChance = motorBike.MelfunctionChance
+            };
+
+            return Ok(updateMotorBike);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public ActionResult<MotorBike> DeleteMotorBike(int id)
+        {
+
+            return Ok($"MotorBike with id {id} was successfully deleted");
+        }
+    }
+}
