39dc4c0ca63a0631a3be0a5e0186a92433aa2ff1
AmitNautiyal <59432251+AmitNautiyalGithub@users.noreply.github.com>
2023-06-23 22:18:20 +0530

    RRW-Create Start Project-feature create car

---
+++ Domain/Entities/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Entities
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string TeamName { get; set; }
+        public double MelfunctionChance { get; set; }
+        public int MelfunctionOccured { get; set; }
+        public int Speed { get; set; }
+        public int DistanceConveredInMiles { get; set; }
+        public int FinishedRace { get; set; }
+        public int RacedForHours { get; set; }
+    }
+}
---
+++ WebUI/Features/Cars/CarsController.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Features.Cars
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Car>> GetCars()
+        {
+            var cars = new List<Car>();
+            Car car1 = new Car
+            {
+                 TeamName = "Team A",
+                 Speed = 100,
+                 MelfunctionChance = 0.2,
+            };
+
+            Car car2 = new Car
+            {
+                TeamName = "Team B",
+                Speed = 90,
+                MelfunctionChance = 0.15,
+            };
+
+            cars.Add(car1);
+            cars.Add(car2);
+
+            return Ok(cars);
+        }
+    }
+}
