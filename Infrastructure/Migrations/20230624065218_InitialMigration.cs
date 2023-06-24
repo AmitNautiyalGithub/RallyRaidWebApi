975fb89647f82424104432148203fb2c4432bc60
AmitNautiyal <59432251+AmitNautiyalGithub@users.noreply.github.com>
2023-06-24 12:43:24 +0530

    RRW-3-Feature: Add ApplicationDbContext and EF

---
+++ Infrastructure/Migrations/20230624065218_InitialMigration.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TeamName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MelfunctionChance = table.Column<double>(type: "float", nullable: false),
+                    MelfunctionOccured = table.Column<int>(type: "int", nullable: false),
+                    Speed = table.Column<int>(type: "int", nullable: false),
+                    DistanceConveredInMiles = table.Column<int>(type: "int", nullable: false),
+                    FinishedRace = table.Column<int>(type: "int", nullable: false),
+                    RacedForHours = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MotorBikes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TeamName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MelfunctionChance = table.Column<double>(type: "float", nullable: false),
+                    MelfunctionOccured = table.Column<int>(type: "int", nullable: false),
+                    Speed = table.Column<int>(type: "int", nullable: false),
+                    DistanceConveredInMiles = table.Column<int>(type: "int", nullable: false),
+                    FinishedRace = table.Column<int>(type: "int", nullable: false),
+                    RacedForHours = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MotorBikes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+
+            migrationBuilder.DropTable(
+                name: "MotorBikes");
+        }
+    }
+}
--- WebUI/Program.cs
+++ WebUI/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//Register dependency Injecton here
 builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddControllers();
